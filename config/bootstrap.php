7196a8c8f4fb528de357cb324a28ce9fe536bbc6
busgurlu <burak@uskur.com.tr>
2018-09-18 16:06:55 +0300

    pathable config

---
+++ config/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright 2010 - 2015, Cake Development Corporation (+1 702 425 5085) (http://cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2010 - 2015, Cake Development Corporation (+1 702 425 5085) (http://cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+use Cake\Core\Configure;
+
+Configure::load('Uskur/CakePHPPathable.pathable');
---
+++ config/pathable.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Copyright 2010 - 2015, Cake Development Corporation (http://cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2010 - 2015, Cake Development Corporation (http://cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+use Cake\Core\Configure;
+
+$config = [
+    'Pathable' => [
+        'community_id' => '',
+        'api_token' => '',
+        'auth_token' => ''
+    ]
+];
+
+return $config;
